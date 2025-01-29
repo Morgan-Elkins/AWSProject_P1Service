ec1d6d055e9e95f592cb6d0079ae166f82e47b67
Morgan-Elkins <me839@york.ac.uk>
2025-01-29 12:03:16 +0000

    Recieved and parsed SQS message

--- app.py
+++ app.py
@@ -1,7 +1,69 @@
+import copy
+import json
+import os
+import time
+
+from dotenv import load_dotenv
+import boto3
 from flask import Flask
 
-app = Flask(__name__)
+load_dotenv()
 
-@app.route("/")
-def say_hello():
-    return "<h1>Hello, Test!</h1>"
+AWS_REGION = os.getenv("AWS_REGION")
+AWS_QUEUE = os.getenv("AWS_Q1")
+sqs = boto3.client('sqs', region_name=AWS_REGION)
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        print(f"{AWS_REGION} {AWS_QUEUE}")
+        return "<h1>test</h1>", 200
+
+    return app
+
+def get_messages():
+    print("TEST")
+    print(f"{AWS_REGION} {AWS_QUEUE}")
+    while True:
+        try:
+            response = sqs.receive_message(
+                QueueUrl=AWS_QUEUE,
+                AttributeNames=[
+                    'SentTimestamp'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ],
+                VisibilityTimeout=0,
+                WaitTimeSeconds=0
+            )
+
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
+
+            # Delete received message from queue
+            sqs.delete_message(
+                QueueUrl=AWS_QUEUE,
+                ReceiptHandle=receipt_handle
+            )
+            print('Received and deleted message: %s' % message)
+
+            body = message['Body']
+            # print("TEST",f"{body.get("title")}", f"{body.get("desc"), }"f"{body.get("prio")}")
+            # print("Message contents %s" % body)
+            body = body.replace("\'", "\"") # WHY?????
+            json_body = json.loads(body)
+            # print(type(json_body))
+            print(f"Message contents {json_body}")
+            print(f"Title: {json_body.get("title")}")
+
+        except:
+            pass
+        time.sleep(1)
+if __name__ == '__main__':
+    # app = create_app()
+    # app.run()
+    get_messages()
