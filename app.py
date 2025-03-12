a37bbd45a1dedd3292f51eb4dc3d7e91608995cc
Morgan-Elkins <me839@york.ac.uk>
2025-03-12 10:05:02 +0000

    Addded print statemtnt output to sending teams message

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def get_messages():
             # if json_body.get('title') is None or json_body.get('desc') is None or json_body.get('prio') is None:
             #     continue
 
-            send_teams_alert(json_body)
+            print(send_teams_alert(json_body))
 
         except:
             pass
